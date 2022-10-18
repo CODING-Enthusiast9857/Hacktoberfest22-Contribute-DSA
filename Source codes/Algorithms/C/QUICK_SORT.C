6fa627c4a86bde49fec74dbc714b196314b7742e
Madhavi Sonawane <108464846+CODING-Enthusiast9857@users.noreply.github.com>
2022-10-18 15:39:40 +0530

    Update QUICK_SORT.C

--- Source codes/Algorithms/C/QUICK_SORT.C
+++ Source codes/Algorithms/C/QUICK_SORT.C
@@ -3,62 +3,54 @@
     It picks an element as key/pivot and partitions the given array around the picked key/pivot.
 
 */
-
-
 #include <stdio.h>
-#define n 5 // can change this size.
 
-void printArray(int arry[])
-//prints the array
-{   
-    printf("The array is: ");
-    for (int i = 0; i < n; i++)
-        printf("%d ", arry[i]);
+void swap(int *a, int *b) {
+  int t = *a;
+  *a = *b;
+  *b = t;
 }
 
-void swap(int i, int j,int arry[])
-// swaps the numbers
-{   int temp;
-    temp = arry[i];
-    arry[i] = arry[j];
-    arry[j] = temp;
-}
-void quicksort(int arry[],int lb,int ub)
-//Creates Parition as well Sorts them in ascending order
-{
-    if (lb<ub)
-    {
-        int key,i,j,flag=1;
-        key=arry[lb];
-        i=lb,j=ub+1;
-
-        while(flag)
-        {   i++;
-            while(arry[i]<key)
-                i++; 
-            j--;
-            while(arry[j]>key)
-                j--;
-            if (i<j)
-                swap(i,j,arry);
-            else
-                flag=0;
-        }
-        swap(lb,j,arry);
-
-        quicksort(arry,lb,j-1);
-        quicksort(arry,j+1,ub);
+int partition(int array[], int low, int high) {
+  int pivot = array[high];
+  int i = (low - 1);
+  for (int j = low; j < high; j++) {
+    if (array[j] <= pivot) {
+      i++;
+      swap(&array[i], &array[j]);
     }
+  }
+
+  swap(&array[i + 1], &array[high]);
+  
+  return (i + 1);
 }
 
+void quickSort(int array[], int low, int high) {
+  if (low < high) {
+    int pi = partition(array, low, high);
+    quickSort(array, low, pi - 1);
+    quickSort(array, pi + 1, high);
+  }
+}
 
-void main()
-{
-    int arry[n],i;
-    int lb=0,ub=n-1;
-    printf("Enter Elements: ");
-    for(i=0;i<n;i++)
-        scanf("%d",&arry[i]);
-    quicksort(arry,lb,ub);
-    printArray(arry);
+void printArray(int array[], int size) {
+  for (int i = 0; i < size; ++i) {
+    printf("%d  ", array[i]);
+  }
+  printf("\n");
+}
+
+int main() {
+  int data[] = {8, 7, 2, 1, 0, 9, 6};
+  
+  int n = sizeof(data) / sizeof(data[0]);
+  
+  printf("Unsorted Array\n");
+  printArray(data, n);
+  
+  quickSort(data, 0, n - 1);
+  
+  printf("Sorted array in ascending order: \n");
+  printArray(data, n);
 }
