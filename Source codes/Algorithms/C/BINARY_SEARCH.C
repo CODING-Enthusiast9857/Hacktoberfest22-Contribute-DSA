3d37fc75e92f12bb384f243af24797e96cdf957d
Madhavi Sonawane <108464846+CODING-Enthusiast9857@users.noreply.github.com>
2022-10-18 15:46:46 +0530

    Update BINARY_SEARCH.C

--- Source codes/Algorithms/C/BINARY_SEARCH.C
+++ Source codes/Algorithms/C/BINARY_SEARCH.C
@@ -1,61 +1,32 @@
-#include<stdio.h>
-#include<conio.h>
-void bubble(int a[],int n);
-void binary(int a[],int n,int s);
-void main()
-{
-	int a[10],i,n,s;
-	clrscr();
-	printf("Enter array size:\n");
-	scanf("%d",&n);
-	printf("Enter array elements:\n");
-	for(i=0;i<n;i++)
-	{
-		scanf("%d",&a[i]);
-	}
-	printf("Array elements are:\n");
-	for(i=0;i<n;i++)
-	{
-		printf("%d\t",a[i]);
-	}
-	printf("\nEnter element to search:\n");
-	scanf("%d",&s);
-	bubble(a,n);
-	binary(a,n,s);
-	getch();
+
+#include <stdio.h>
+
+int binarySearch(int array[], int x, int low, int high) {
+  // Repeat until the pointers low and high meet each other
+  while (low <= high) {
+    int mid = low + (high - low) / 2;
+
+    if (array[mid] == x)
+      return mid;
+
+    if (array[mid] < x)
+      low = mid + 1;
+
+    else
+      high = mid - 1;
+  }
+
+  return -1;
 }
-void bubble(int a[],int n)
-{
-	int i,j,t;
-	for(i=0;i<n-1;i++)
-	{
-		for(j=0;j<n-1-i;j++)
-		{
-			if(a[j]>a[j+1])
-			{
-				t=a[j];
-				a[j]=a[j+1];
-				a[j+1]=t;
-			}
-		}
-	}
-}
-void binary(int a[],int n,int s)
-{
-	int beg,end,mid,loc;
-	beg=0;
-	end=n-1;
-	mid=(beg+end)/2;
-	while(a[mid]!=s && beg<=end)
-	{
-		if(a[mid]<s)
-		beg=mid+1;
-		else
-		end=mid-1;
-		mid=(beg+end)/2;
-	}
-	if(a[mid]==s)
-	printf("%d is found at %d position",s,mid);
-	else
-	printf("%d is not found",s);
+
+int main(void) {
+  int array[] = {3, 4, 5, 6, 7, 8, 9};
+  int n = sizeof(array) / sizeof(array[0]);
+  int x = 4;
+  int result = binarySearch(array, x, 0, n - 1);
+  if (result == -1)
+    printf("Not found");
+  else
+    printf("Element is found at index %d", result);
+  return 0;
 }
