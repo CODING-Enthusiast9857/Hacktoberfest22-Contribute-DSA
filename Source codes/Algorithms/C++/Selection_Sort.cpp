c93aa769af54fb1764057f6f1936e348b9ebe054
Aditya093 <90855562+Aditya093@users.noreply.github.com>
2022-10-19 19:20:35 +0530

    Update Selection_Sort.cpp

--- Source codes/Algorithms/C++/Selection_Sort.cpp
+++ Source codes/Algorithms/C++/Selection_Sort.cpp
@@ -2,9 +2,10 @@
 using namespace std;
 void selection_sort(int arr[],int size)
 {
-    for(int i=0;i<size;i++)
+    for(int i=0;i<size-1;i++)
     {
-        int min_index=i,temp;
+        int min_index=i;
+        int temp;
         for(int j=i+1;j<size;j++)
         {
              if(arr[j]<arr[min_index])
@@ -12,23 +13,28 @@ void selection_sort(int arr[],int size)
                  min_index=j;
              }
         }
-        temp=arr[min_index];
-        arr[min_index]=arr[i];
-        arr[i]=temp;
+        if(min_index!=i) {
+            temp = arr[min_index];
+            arr[min_index] = arr[i];
+            arr[i] = temp;
+        }
     }
 }
 
 int main()
 {
-    int arr[10];
+    int n;
+    cout<<"Enter the number of elements you want in the array:";
+    cin>>n;
+    int arr[n];
     cout<<"Enter the elements:";
-    for(int i=0;i<10;i++)
+    for(int i=0;i<n;i++)
     {
         cin>>arr[i];
     }
-    selection_sort(arr,10);
+    selection_sort(arr,n);
     cout<<"Sorted Elements are:";
-    for(int i=0;i<10;i++)
+    for(int i=0;i<n;i++)
     {
         cout<<arr[i]<<" ";
     }
