02300cd3a53f9d6cbbb67fe3511544a39a195373
Roshan Lamichhane <33913826+roshanlam@users.noreply.github.com>
2022-10-19 17:10:15 -0400

    Update Trie.java
    Rewrote Trie.java and tested it on replit, all good to go,

--- Source codes/Data Structures/Java/Trie.java
+++ Source codes/Data Structures/Java/Trie.java
@@ -1,79 +1,55 @@
-public class Trie {
-    static final int size = 26;
-    static class TrieNode{
-        TrieNode[] children = new TrieNode[size];
-        boolean isEndOfWord;
-        TrieNode(){
-            isEndOfWord = false;
-            for(int i = 0; i < size; i++){
-                children[i] = null;
-            }
-        };
+import java.util.*;
 
-        static TrieNode root;
+class TrieNode {
+	boolean endOfWord;
+	Map<Character, TrieNode> children;
 
-        static void insert(String key){
-            int level;
-            int length = key.length();
-            int index;
+	public TrieNode() {
+		endOfWord = false;
+		children = new HashMap<>();
+	}
+}
 
-            TrieNode pCrawl = root;
-            for(level = 0; level < length; level++){
-                index = key.charAt(level) - 'a';
-                if (pCrawl.children[index] == null)
-                    pCrawl.children[index] = new TrieNode();
+class Trie {
+	TrieNode root;
+	public Trie() {
+		root = new TrieNode();
+	}
 
-                pCrawl = pCrawl.children[index];
-            }
-            pCrawl.isEndOfWord = true;
-        }
+	public void insert(String word) {
+		TrieNode temp = root;
+		for (char letter : word.toCharArray()) {
+			if (!temp.children.containsKey(letter)) {
+				temp.children.put(letter, new TrieNode());
+			}
+			temp = temp.children.get(letter);
+		}
+		temp.endOfWord = true;
+	}
 
-        static boolean search(String key){
-            int level;
-            int length = key.length();
-            int index;
+	public boolean search(String word) {
+		TrieNode temp = root;
 
-            TrieNode pCrawl = root;
+		for (char letter : word.toCharArray()) {
+			if (temp.children.containsKey(letter)) {
+				temp = temp.children.get(letter);
+			} else {
+				return false;
+			}
+		}
+		return temp.endOfWord;
+	}
 
-            for(level = 0; level < length; level++){
-                index = key.charAt(level) - 'a';
+	public boolean startsWith(String prefix) {
+		TrieNode temp = root;
 
-                if (pCrawl.children[index] == null)
-                    return false;
-
-                pCrawl = pCrawl.children[index];
-            }
-            return (pCrawl.isEndOfWord);
-        }
-
-        public static void main(String[] args) {
-            String[] keys = {"the", "a", "there", "answer", "any",
-                    "by", "bye", "their"};
-            String[] output = {"Not present in trie", "Present in trie"};
-
-            root = new TrieNode();
-
-            // Construct trie
-            int i;
-            for (i = 0; i < keys.length ; i++)
-                insert(keys[i]);
-
-            // Search for different keys
-            if(search("the"))
-                System.out.println("the --- " + output[1]);
-            else System.out.println("the --- " + output[0]);
-
-            if(search("these"))
-                System.out.println("these --- " + output[1]);
-            else System.out.println("these --- " + output[0]);
-
-            if(search("their"))
-                System.out.println("their --- " + output[1]);
-            else System.out.println("their --- " + output[0]);
-
-            if(search("thaw"))
-                System.out.println("thaw --- " + output[1]);
-            else System.out.println("thaw --- " + output[0]);
-        }
-    }
+		for (char letter : prefix.toCharArray()) {
+			if (temp.children.containsKey(letter)) {
+				temp = temp.children.get(letter);
+			} else {
+				return false;
+			}
+		}
+		return true;
+	}
 }
