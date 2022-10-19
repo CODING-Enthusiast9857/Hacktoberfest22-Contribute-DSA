cf15e14f3c95cb21e5fa33209501b29aef6c8da1
Madhavi Sonawane <108464846+CODING-Enthusiast9857@users.noreply.github.com>
2022-10-19 17:12:52 +0530

    Update DNF_sort.cpp

--- Source codes/Algorithms/C++/DNF_sort.cpp
+++ Source codes/Algorithms/C++/DNF_sort.cpp
@@ -38,8 +38,10 @@ void dnfsort(int arr[], int n){
 
 int main(){
     int n;
+    cout<<"Enter no. of elements : ";
     cin>>n;
     int arr[n];
+    cout<<"Enter elements : ";
     for(int i=0; i<n; i++){
         cin>>arr[i];
     }
