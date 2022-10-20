27c9644de290152112b227b2df2fb7e09e8510df
MD DANISH JAMAL <81770608+imdj6@users.noreply.github.com>
2022-10-20 12:37:49 +0530

    added dutch-national-flag problem in cpp

---
+++ Source codes/Algorithms/C++/DutchNationalFlagproblem.cpp
@@ -0,0 +1,72 @@
+/*
+Given N balls of colour red, white or blue arranged in a line in random order. You have to arrange all the balls such that the balls with the same colours are adjacent with the order of the balls, with the order of the colours being red, white and blue (i.e., all red coloured balls come first then the white coloured balls and then the blue coloured balls). 
+
+Input: {0, 1, 2, 0, 1, 2}
+Output: {0, 0, 1, 1, 2, 2}
+
+Input: {0, 1, 1, 0, 1, 2, 1, 2, 0, 0, 0, 1}
+Output: {0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 2, 2}
+
+similar problem:
+Sort an array of 0s, 1s and 2s
+*/
+
+// C++ program to sort an array
+// with 0, 1 and 2 in a single pass
+#include <bits/stdc++.h>
+using namespace std;
+
+// Function to sort the input array,
+// the array is assumed
+// to have values in {0, 1, 2}
+void sort012(int a[], int arr_size)
+{
+	int lo = 0;
+	int hi = arr_size - 1;
+	int mid = 0;
+
+	// Iterate till all the elements
+	// are sorted
+	while (mid <= hi) {
+		switch (a[mid]) {
+
+		// If the element is 0
+		case 0:
+			swap(a[lo++], a[mid++]);
+			break;
+
+		// If the element is 1 .
+		case 1:
+			mid++;
+			break;
+
+		// If the element is 2
+		case 2:
+			swap(a[mid], a[hi--]);
+			break;
+		}
+	}
+}
+
+// Function to print array arr[]
+void printArray(int arr[], int arr_size)
+{
+	// Iterate and print every element
+	for (int i = 0; i < arr_size; i++)
+		cout << arr[i] << " ";
+}
+
+// Driver Code
+int main()
+{
+	int arr[] = { 0, 1, 1, 0, 1, 2, 1, 2, 0, 0, 0, 1 };
+	int n = sizeof(arr) / sizeof(arr[0]);
+
+	sort012(arr, n);
+
+	printArray(arr, n);
+
+	return 0;
+}
+
+
