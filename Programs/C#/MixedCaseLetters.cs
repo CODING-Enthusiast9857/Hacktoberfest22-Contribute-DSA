54cae07a2e7e098d0a382c73c14f6794c74eb980
Ebuka Collins <52864306+ebukaracer@users.noreply.github.com>
2022-10-20 13:20:41 +0100

    Create MixedCaseLetters.cs
    Program to replace lower-case characters with upper-case and vice versa.
    Create folder for C# language codes.

---
+++ Programs/C#/MixedCaseLetters.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Text;
+
+/// <summary>
+/// ThIs Is An ExAmPlE oF mIxEd CaSe WoRdS
+/// </summary>
+internal class MixedCaseLetters
+{
+    private static void MixCase(string text = "dEfAuLt")
+    {
+        if (string.IsNullOrWhiteSpace(text))
+            return;
+
+        StringBuilder sb = new StringBuilder();
+
+        for (int i = 0; i < text.Length; i++)
+        {
+            if (char.ToUpper(text[i]) == text[i])
+                sb.Append(char.ToLower(text[i]));
+
+            else if (char.ToLower(text[i]) == text[i])
+                sb.Append(char.ToUpper(text[i]));
+        }
+
+        Console.WriteLine($"Original Text: {text}\n" +
+                          $"Converted Text: {sb}");
+    }
+}
