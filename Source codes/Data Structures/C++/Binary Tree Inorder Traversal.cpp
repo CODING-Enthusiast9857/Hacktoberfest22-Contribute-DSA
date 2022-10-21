2470fdb54f269f895e48562ad479d06f83e657a9
Ankit <ankit.kumar730007@gmail.com>
2022-10-21 13:39:24 +0530

    binary tree

--- Source codes/Data Structures/C++/Binary Tree Inorder Traversal.cpp
+++ Source codes/Data Structures/C++/Binary Tree Inorder Traversal.cpp
@@ -1,68 +1,164 @@
-#include <iostream>
+# include <iostream>
+# include <cstdlib>
 using namespace std;
+/*
+ * Node Declaration
+ */
+struct node
+{
+    int info;
+    struct node *left;
+    struct node *right;
+}*root;
  
- 
-struct Node {
-    int data;
-    struct Node* next;
-    Node(int data)
-    {
-        this->data = data;
-        next = NULL;
-    }
-};
- 
-struct LinkedList {
-    Node* head;
-    LinkedList()
-    {
-        head = NULL;
-    }
- 
-    Node* reverse(Node* head)
-    {
-        if (head == NULL || head->next == NULL)
-            return head;
-        // Recursive call
-        Node* rest = reverse(head->next);
-        head->next->next = head;
-        
-        head->next = NULL;
- 
-        return rest;
-    }
- 
-    void print()
-    {
-        struct Node* temp = head;
-        while (temp != NULL) {
-            cout << temp->data << " ";
-            temp = temp->next;
+/*
+ * Class Declaration
+ */
+class BST
+{
+    public:
+        void insert(node *, node *);
+        void inorder(node *);
+        void display(node *, int);
+        BST()
+        {
+            root = NULL;
         }
-    }
- 
-    void push(int data)
-    {
-        Node* temp = new Node(data);
-        temp->next = head;
-        head = temp;
-    }
 };
- 
+/*
+ * Main Contains Menu
+ */
 int main()
 {
-    LinkedList ll;
-    ll.push(320);
-    ll.push(34);
-    ll.push(315);
-    ll.push(385);
+    int choice, num;
+    BST bst;
+    node *temp;
+    while (1)
+    {
+        cout<<"-----------------"<<endl;
+        cout<<"Operations on BST"<<endl;
+        cout<<"-----------------"<<endl;
+        cout<<"1.Insert Element "<<endl;
+        cout<<"2.Inorder Traversal"<<endl;
+        cout<<"3.Display"<<endl;
+        cout<<"4.Quit"<<endl;
+        cout<<"Enter your choice : ";
+        cin>>choice;
+        switch(choice)
+        {
+        case 1:
+            temp = new node;
+            cout<<"Enter the number to be inserted : ";
+            cin>>temp->info;
+            bst.insert(root, temp);
+            break;
+        case 2:
+            cout<<"Inorder Traversal of BST:"<<endl;
+            bst.inorder(root);
+            cout<<endl;
+            break;
+        case 3:
+            cout<<"Display BST:"<<endl;
+            bst.display(root,1);
+            cout<<endl;
+            break;
+        case 4:
+            exit(1);
+        default:
+            cout<<"Wrong choice"<<endl;
+        }
+    }
+}
  
-    cout << "Linked List Before Reversing\n";
-    ll.print();
  
-    ll.head = ll.reverse(ll.head);
+/*
+ * Inserting Element into the Tree
+ */
+void BST::insert(node *tree, node *newnode)
+{
+    if (root == NULL)
+    {
+        root = new node;
+        root->info = newnode->info;
+        root->left = NULL;
+        root->right = NULL;
+        cout<<"Root Node is Added"<<endl;
+        return;
+    }
+    if (tree->info == newnode->info)
+    {
+        cout<<"Element already in the tree"<<endl;
+        return;
+    }
+    if (tree->info > newnode->info)
+    {
+        if (tree->left != NULL)
+        {
+            insert(tree->left, newnode);
+        }
+        else
+        {
+            tree->left = newnode;
+            (tree->left)->left = NULL;
+            (tree->left)->right = NULL;
+            cout<<"Node Added To Left"<<endl;
+            return;
+        }
+    }
+    else
+    {
+        if (tree->right != NULL)
+        {
+            insert(tree->right, newnode);
+        }
+        else
+        {
+            tree->right = newnode;
+            (tree->right)->left = NULL;
+            (tree->right)->right = NULL;
+            cout<<"Node Added To Right"<<endl;
+            return;
+        }
+    }
+}
  
-    cout << "\nLinked List After Reversing \n";
-    ll.print();
-    return 0;
+/*
+ * In Order Traversal
+ */
+void BST::inorder(node *ptr)
+{
+    if (root == NULL)
+    {
+        cout<<"Tree is empty"<<endl;
+        return;
+    }
+    if (ptr != NULL)
+    {
+        inorder(ptr->left);
+        cout<<ptr->info<<"  ";
+        inorder(ptr->right);
+    }
+}
+ 
+ 
+/*
+ * Display Tree Structure
+ */
+void BST::display(node *ptr, int level)
+{
+    int i;
+    if (ptr != NULL)
+    {
+        display(ptr->right, level+1);
+        cout<<endl;
+        if (ptr == root)
+            cout<<"Root->:  ";
+        else
+        {
+            for (i = 0;i < level;i++)
+                cout<<"       ";
+	}
+        cout<<ptr->info;
+        display(ptr->left, level+1);
+    }
 }
