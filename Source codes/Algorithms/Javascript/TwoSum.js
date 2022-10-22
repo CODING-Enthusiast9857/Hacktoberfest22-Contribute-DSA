5253888a1ecbdbfc1efea8bee614f1aeb1ecef4a
Morenike Oyewole <herroyalpianist@gmail.com>
2022-10-22 08:21:00 +0100

    javascript two-sum algorithm

---
+++ Source codes/Algorithms/Javascript/TwoSum.js
@@ -0,0 +1,15 @@
+// time complexity =  O(n); space complexity = O(n)
+var twoSum = function (nums, target) {
+  const map = new Map();
+
+  for (let i = 0; i < nums.length; i++) {
+    const number = nums[i];
+    const complement = target - number;
+
+    if (map.has(complement)) {
+      return [i, map.get(complement)];
+    }
+
+    map.set(number, i);
+  }
+};
