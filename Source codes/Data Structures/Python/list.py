dfdc0c33d02916c36b642d2473dd6ff674a8c58f
Dilpreet <113202190+Dilpreet1409@users.noreply.github.com>
2022-10-22 15:36:42 +0530

    Update list.py

--- Source codes/Data Structures/Python/list.py
+++ Source codes/Data Structures/Python/list.py
@@ -6,3 +6,24 @@ for i in range(n):
 print("List elements are :")
 for j in l1:
     print(j)
+    
+print("Accessing element from the list by using indexing")
+print(l1[1]) 
+print(l1[2])
+
+#Printing the largest number in the list 
+largest = l1[0]
+for i in l1:
+    if i > largest :
+      largest = i 
+print(f"{largest} is the largest number from the list.")
+
+#Printing the list in reverse order
+reverse=[]
+for i in l1[::-1]:
+    reverse = reverse + [i]
+print(reverse)
+
+
+
+   
